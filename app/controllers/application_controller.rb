6a475148b73f7b378b295dadc715e6bc88bf9ebd
Chris Wade <chris@wadespace.net>
2021-08-19 16:07:05 +1000

    login werks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
-  # protect_from_forgery
-  # skip_before_action :verify_authenticity_token
 
   # user lamba gives current user
   inertia_share user: -> { current_user }
-
-  # Logout Path
-  # def after_sign_out_path_for(_resource_or_scope)
-  #   # raise 'SIGN IN TEST'
-  #   redirect_to new_user_session_path
-  # end
 
   inertia_share flash: lambda {
     {
