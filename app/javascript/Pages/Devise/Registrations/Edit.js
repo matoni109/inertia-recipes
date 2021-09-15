9918f62fc69d68229d9b16e0555b27c54803072f
Chris Wade <chris@wadespace.net>
2021-09-15 17:42:42 +1000

    update

--- app/javascript/Pages/Devise/Registrations/Edit.js
+++ app/javascript/Pages/Devise/Registrations/Edit.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useForm } from "@inertiajs/inertia-react";
 import Layout from "~/Components/Layout";
+import AvatarCloudinary from "~/Components/AvatarCloudinary";
 
 const Edit = (props) => {
-  // console.log(props.user);
+  const inputFileRef = useRef(null);
+  // console.log(props.data.avatar);
   // const {
   //   props: { user, flash },
   // } = usePage();
   const { data, setData, patch, processing, errors } = useForm({
     user: {
+      avatar: null,
       name: props.user.name,
       email: props.user.email,
       password: "",
@@ -17,11 +20,36 @@ const Edit = (props) => {
     },
   });
 
+  const onFilechange = (event) => {
+    /*Selected files data can be collected here.*/
+    // console.log(event);
+    console.log(event.target.id);
+    console.log(event.target.files[0]);
+    setData((values) => ({
+      user: { ...values.user, [event.target.id]: event.target.files[0] },
+    }));
+    console.log(data.user.avatar);
+  };
+  const onBtnClick = () => {
+    /*Collecting node-element and performing click*/
+    inputFileRef.current.click();
+  };
   // [:password, :password_confirmation, :current_password]
   const valuesChangedHandler = (event) => {
     setData((values) => ({
       user: { ...values.user, [event.target.id]: event.target.value },
     }));
+  };
+
+  const avatarHandler = (event) => {
+    // console.log(event.target.files[0]);
+    console.log(event.target.id);
+    console.log(event.target.files[0]);
+    setData((values) => ({
+      user: { ...values.user, [event.target.id]: event.target.files[0] },
+    }));
+    console.log(data.user.avatar);
+    console.log(data.user);
   };
 
   function submit(e) {
@@ -53,7 +81,48 @@ const Edit = (props) => {
               </h1>
               {/* <AlertForm props={flash} /> */}
               <div className="mx-auto mt-6 w-24 border-b-2" />
-              <label htmlFor="text-input-11" className="form-label">
+              <div>
+                <label className="form-label">Photo:</label>
+                <div className="mt-1 flex items-center">
+                  <span className="inline-block h-15 w-15 rounded-full overflow-hidden bg-gray-100">
+                    {/* <svg
+                      className="h-full w-full text-gray-300"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg> */}
+                    <AvatarCloudinary userData={props.data.avatar} />
+                  </span>
+                  {/* <input
+                    type="file"
+                    id="avatar"
+                    className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    // value={data.user.avatar}
+                    // onChange={(e) => setData("avatar", e.target.files[0])}
+                    onChange={avatarHandler}
+                  /> */}
+                  <input
+                    type="file"
+                    id="avatar"
+                    ref={inputFileRef}
+                    // onChange={onFilechange}
+                    className=""
+                    onChange={avatarHandler}
+                  />
+                  <button id="avatarButton" onClick={onBtnClick}>
+                    Select file
+                  </button>
+                  {/* <button
+                    type="button"
+                    onClick={avatarHandler}
+                    className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Change
+                  </button> */}
+                </div>
+              </div>
+              <label htmlFor="text-input-11" className="form-label my-2">
                 Name:
               </label>
               <input
